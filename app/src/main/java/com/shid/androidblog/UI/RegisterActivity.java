36532db07efadee0e00926d4ced38da861ffb579
Shid Insa <theshid.insa@gmail.com>
2020-04-09 04:35:14 +0100

    edit

--- app/src/main/java/com/shid/androidblog/UI/RegisterActivity.java
+++ app/src/main/java/com/shid/androidblog/UI/RegisterActivity.java
@@ -1,16 +1,78 @@
 package com.shid.androidblog.UI;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.shid.androidblog.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView userImg;
+    static int permissionRequestCode = 1;
+    static int REQUEST_CODE = 1;
+    private Uri imgChosenURI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        userImg = findViewById(R.id.register_user_photo);
+
+        userImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= 22){
+                     checkRequestPermission();
+                } else {
+                    openGallery();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE && data != null){
+
+            imgChosenURI = data.getData();
+            userImg.setImageURI(imgChosenURI);
+        }
+    }
+
+    private void openGallery() {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUEST_CODE);
+    }
+
+    private void checkRequestPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(this, "Accept for required permission", Toast.LENGTH_SHORT).show();
+            } else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        permissionRequestCode);
+            }
+        } else{
+            openGallery();
+        }
     }
 }
