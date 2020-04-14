427a3f3b3a15630cadac4a80e270eba8ae42b246
Shid Insa <theshid.insa@gmail.com>
2020-04-14 02:08:51 +0100

    edit

--- app/src/main/java/com/shid/androidblog/UI/RegisterActivity.java
+++ app/src/main/java/com/shid/androidblog/UI/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.shid.androidblog.UI;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -12,9 +13,22 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.shid.androidblog.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -24,18 +38,53 @@ public class RegisterActivity extends AppCompatActivity {
     static int REQUEST_CODE = 1;
     private Uri imgChosenURI;
 
+    private EditText userEmail, userName, userPassword, userPassword2;
+    private ProgressBar loadingProgress;
+    private Button btnReg;
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         userImg = findViewById(R.id.register_user_photo);
+        userEmail = findViewById(R.id.register_mail);
+        userName = findViewById(R.id.register_name);
+        userPassword = findViewById(R.id.register_password);
+        userPassword2 = findViewById(R.id.register_password2);
+        loadingProgress = findViewById(R.id.progressBar);
+        loadingProgress.setVisibility(View.INVISIBLE);
+        btnReg = findViewById(R.id.register_btn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnReg.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userPassword2.getText().toString();
+                final String name = userName.getText().toString();
+
+                if (email.isEmpty() || name.isEmpty() || password.isEmpty() || !password.equals(password2)) {
+                    showMessage("Please verify all fields");
+                    btnReg.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                } else {
+                    createAccount(email, name, password);
+                }
+            }
+        });
 
         userImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (Build.VERSION.SDK_INT >= 22){
-                     checkRequestPermission();
+                if (Build.VERSION.SDK_INT >= 22) {
+                    checkRequestPermission();
                 } else {
                     openGallery();
                 }
@@ -43,11 +92,73 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    private void createAccount(String email, final String name, String password) {
+        
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            showMessage("Account Created!");
+                            updateUserInfo(name,imgChosenURI, firebaseAuth.getCurrentUser());
+                        }
+                        else{
+                            showMessage("Account Creation Failed" + task.getException().getMessage());
+                            btnReg.setVisibility(View.VISIBLE);
+                            loadingProgress.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+    }
+
+    private void updateUserInfo(final String name, Uri imgChosenURI, final FirebaseUser currentUser) {
+
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        final StorageReference imageFilePath = mStorage.child(imgChosenURI.getLastPathSegment());
+        imageFilePath.putFile(imgChosenURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                //image loaded, we retrieve the image url
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
+
+                        currentUser.updateProfile(profileUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            showMessage("Registration Complete");
+                                            updateUI();
+                                        }
+                                    }
+                                });
+                    }
+                });
+            }
+        });
+    }
+
+    private void updateUI() {
+        Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE && data != null){
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE && data != null) {
 
             imgChosenURI = data.getData();
             userImg.setImageURI(imgChosenURI);
@@ -58,20 +169,20 @@ public class RegisterActivity extends AppCompatActivity {
 
         Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
         galleryIntent.setType("image/*");
-        startActivityForResult(galleryIntent,REQUEST_CODE);
+        startActivityForResult(galleryIntent, REQUEST_CODE);
     }
 
     private void checkRequestPermission() {
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-        != PackageManager.PERMISSION_GRANTED){
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
                 Toast.makeText(this, "Accept for required permission", Toast.LENGTH_SHORT).show();
-            } else{
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                         permissionRequestCode);
             }
-        } else{
+        } else {
             openGallery();
         }
     }
